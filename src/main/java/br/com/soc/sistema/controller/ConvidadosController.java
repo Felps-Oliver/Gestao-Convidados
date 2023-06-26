66e0e9dc8d579034f480d783546e9f159816a474
Ésspio <felipe.oliveira200085@gmail.com>
2023-06-26 17:50:17 -0300

    Enviando objeto do Controller para a View

--- src/main/java/br/com/soc/sistema/controller/ConvidadosController.java
+++ src/main/java/br/com/soc/sistema/controller/ConvidadosController.java
@@ -1,14 +1,32 @@
 package br.com.soc.sistema.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.soc.sistema.repository.Convidados;
 
 @Controller
 public class ConvidadosController {
 	
+	@Autowired
+	private Convidados convidados;
+	
+	/*Do modo abaixo, apenas retornará a View, porém sem nenhum dado
+	 * passado do Controller para a View*/
+	
+//	@GetMapping(path = "/convidados")
+//	public String listar() {
+//		return "ListaConvidados";
+//	}
+	
+	/*Deste modo, passam-se dados do Controller para a View, dizendo
+	 * qual a página a ser renderizada*/
 	@GetMapping(path = "/convidados")
-	public String listar() {
-		return "ListaConvidados";
+	public ModelAndView listar() {
+		ModelAndView modelAndView = new ModelAndView("ListaConvidados");
+		modelAndView.addObject("convidados", convidados.findAll());
+		return modelAndView;
 	}
-
 }
