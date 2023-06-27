4c45e67fa446c50c2a6e58e5eabdd468e8025b45
Ésspio <felipe.oliveira200085@gmail.com>
2023-06-27 17:43:21 -0300

    Ajuste no ConvidadosController, modificando para utilizar o RequestMapping

--- src/main/java/br/com/soc/sistema/controller/ConvidadosController.java
+++ src/main/java/br/com/soc/sistema/controller/ConvidadosController.java
@@ -4,12 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.soc.sistema.model.Convidado;
 import br.com.soc.sistema.repository.Convidados;
 
 @Controller
+/* Deste modo, anotando a classe com o RequestMapping, todos os métodos (endpoints) terão automaticamente o
+ * prefixo "/convidados*/
+@RequestMapping("/convidados")
 public class ConvidadosController {
 
 	@Autowired
@@ -29,7 +33,7 @@ public class ConvidadosController {
 	 * Deste modo, passam-se dados do Controller para a View, dizendo qual a página
 	 * a ser renderizada
 	 */
-	@GetMapping(path = "/convidados")
+	@GetMapping
 	public ModelAndView listar() {
 		ModelAndView modelAndView = new ModelAndView("ListaConvidados");
 		modelAndView.addObject(new Convidado());
@@ -37,7 +41,7 @@ public class ConvidadosController {
 		return modelAndView;
 	}
 
-	@PostMapping("/convidados")
+	@PostMapping
 	public String salvar(Convidado convidado) {
 		this.convidados.save(convidado);
 		return "redirect:/convidados";
